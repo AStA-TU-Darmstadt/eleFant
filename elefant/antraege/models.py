25b7d49450fba45b0c216583bb4c2d807cea92bb
Julian Haas <mail@julian-haas.de>
2017-03-26 17:09:47 +0200

    started to define models

--- elefant/antraege/models.py
+++ elefant/antraege/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Application(models.Model):  # Finanzantrag
+    applicant = models.CharField(max_length=100)  # Antragsteller*in
+    number = models.CharField(primary_key=True, max_length=20)  # FA-Nummer
+    application_date = models.DateTimeField('date of application')  # Datum der Antragstellung
+    contact_information = models.CharField(max_length=100)
+    bank_account = models.ForeignKey(BankAccount, on_delete=models.PROTECT)
+    description = models.TextField(max_length=4000)
+
+    def __str__(self):
+        return self.number
+
+
+class BankAccount(models.Model):

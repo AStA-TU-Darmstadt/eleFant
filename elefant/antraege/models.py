41ce46e51aa2ec65f1422e2e91f08fd7656eb49e
Julian Haas <mail@julian-haas.de>
2017-03-27 01:25:00 +0200

    first version of the UI

--- elefant/antraege/models.py
+++ elefant/antraege/models.py
@@ -6,11 +6,11 @@ class Application(models.Model):  # Finanzantrag
     number = models.CharField(primary_key=True, max_length=20)  # FA-Nummer
     application_date = models.DateTimeField('date of application')  # Datum der Antragstellung
     contact_information = models.CharField(max_length=100)
-    bank_account = models.ForeignKey(BankAccount, on_delete=models.PROTECT)
+    # bank_account = models.ForeignKey(BankAccount, on_delete=models.PROTECT)
     description = models.TextField(max_length=4000)
 
     def __str__(self):
         return self.number
 
 
-class BankAccount(models.Model):
+# class BankAccount(models.Model):
--- elefant/antraege/views.py
+++ elefant/antraege/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
 
-# Create your views here.
+from .models import Application
+
+
+class IndexView(generic.ListView):
+    template_name = 'antraege/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Application.objects

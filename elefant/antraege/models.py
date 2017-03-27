59772bad2b6d3d77affc28fdb4bb93479b0e1387
Julian Haas <mail@julian-haas.de>
2017-03-27 16:59:46 +0200

    add contact and total_value to Application model

--- elefant/antraege/models.py
+++ elefant/antraege/models.py
@@ -3,14 +3,18 @@ from django.db import models
 
 class Application(models.Model):  # Finanzantrag
     applicant = models.CharField(max_length=100)  # Antragsteller*in
+    contact = models.CharField(max_length=100)  # Ansprechpartner*in
+    contact_information = models.CharField(max_length=100)
+
     number = models.CharField(primary_key=True, max_length=20)  # FA-Nummer
     application_date = models.DateTimeField('date of application')  # Datum der Antragstellung
-    contact_information = models.CharField(max_length=100)
-    # bank_account = models.ForeignKey(BankAccount, on_delete=models.PROTECT)
+
+    bank_account = models.ForeignKey(BankAccount, on_delete=models.PROTECT)
+
     description = models.TextField(max_length=4000)
+    total_value = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
         return self.number
-
 
 # class BankAccount(models.Model):

2ba41b0727612201feb1a065a040f607e8bfd583
Julian <github@julian-haas.de>
2017-05-02 15:52:14 +0200

    Alpha 0.2 (#6)
    * add build script to package.json
    
    * fix errors when using uwsgi
    
    * use class based views
    
    * use form factory instead of custom model forms
    
    * improve drawer layout on small screens
    
    * add list view for applications
    
    * add header for small screens
    
    * add user login and logout
    
    * remove ugly js fix for required fields
    
    * remove BIC from bank accounts
    
    * shorten offene Rückfragen to Rückfragen
    
    * clean index (remove inline css and fix scaling problems on mobile devices)
    
    TG-66 #closed
    
    * use nice fonts
    
    TG-45 #closed
    
    * new logo
    
    TG-44 #closed
    
    * style improvements
    
    * style improvements #2
    
    * new logo and static file cleanup
    
    * reduce font size in nav
    
    * add static files and licences to repo
    
    * use raleway medium
    
    * rename antraege to elefant
    
    * reorganize folder structure
    
    * remove example_project
    
    * remove manage.py
    
    * rename django to src
    
    * move license to src folder
    
    * Revert "move license to src folder"
    
    This reverts commit c203d2e0edf825ed63555043ab8179d967d3d80c.
    
    * make LICENSE a txt file
    
    * make LICENSE a txt file
    
    * Revert "make LICENSE a txt file"
    
    This reverts commit c13df955040ca86819215ce9cbf3dcc8658f241a.
    
    * end license testing

---
+++ src/elefant/views.py
@@ -0,0 +1,151 @@
+from uuid import UUID
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import ObjectDoesNotExist
+from django.forms import modelform_factory
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .forms import *
+
+
+class IndexView(generic.ListView):
+    template_name = 'elefant/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Application.objects
+
+
+def new_application(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create form instances #and populate them with data from the request:
+        bank_account_form = BankAccountForm(request.POST)
+        application_form = ApplicationForm(request.POST)
+
+        # check whether the application is valid:
+        if application_form.is_valid():
+            # check if bank account already exists, if yes update it
+            try:
+                bank_account = BankAccount.objects.get(iban=request.POST['iban'])
+                bank_account_form = BankAccountForm(request.POST, instance=bank_account)
+            except ObjectDoesNotExist:
+                pass
+            bank_account = bank_account_form.save()  # this validates the form
+
+            # generate initial value for the application
+            application = Application(application_date=timezone.now(), bank_account=bank_account)
+            application.generate_application_number()
+
+            application_form = ApplicationForm(request.POST, instance=application)
+            application_form.save()
+            return render(request, 'elefant/submitted_successfully.html',
+                          {'application_form': application_form, 'bank_account_form': bank_account_form})
+    else:
+        # if a GET (or any other method) we'll create a blank form
+        application_form = ApplicationForm()
+        bank_account_form = BankAccountForm()
+    return render(request, 'elefant/application_form.html',
+                  {'application_form': application_form, 'bank_account_form': bank_account_form})
+
+
+def search_application(request):
+    if request.method == 'POST':
+        try:
+            reference_number = UUID(request.POST['reference_number'])  # check if ref_nr is a valid uuid
+            if Application.objects.filter(pk=reference_number).exists():  # check if application with this ref_nr exists
+                return HttpResponseRedirect(reverse('elefant:detail', args=(reference_number,)))
+        except ValueError:
+            pass
+    return render(request, 'elefant/search_application.html')
+
+
+class ApplicationCreate(generic.CreateView):
+    model = Application
+    fields = ['applicant', 'contact', 'e_mail', 'description', 'total_amount']
+
+    # generate empty bank account form
+    bank_account_form = modelform_factory(BankAccount, fields="__all__")
+
+    def get_context_data(self, **kwargs):
+        # add bank account form to the context to make it usable in the template
+        context = super(ApplicationCreate, self).get_context_data(**kwargs)
+        context['bank_account_form'] = self.bank_account_form
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handles POST requests, instantiating a form instance and its inline
+        formsets with the passed POST variables and then checking them for
+        validity.
+        """
+        # process bank_account form
+        try:
+            # check if this bank account is already in the system
+            bank_account = BankAccount.objects.get(iban=request.POST['iban'])
+            self.bank_account_form = self.bank_account_form(request.POST, instance=bank_account)
+        except ObjectDoesNotExist:
+            self.bank_account_form = self.bank_account_form(request.POST)
+
+        if self.bank_account_form.is_valid():
+            # proceed with standard implementation
+            return super(ApplicationCreate, self).post(self, request, *args, **kwargs)
+        else:
+            return super(ApplicationCreate, self).form_invalid(self.get_form())
+
+    def form_valid(self, form):
+        # save bank account
+        bank_account = self.bank_account_form.save()
+
+        # generate initial values for the new application
+        # noinspection PyAttributeOutsideInit
+        self.object = Application(application_date=timezone.now(), bank_account=bank_account)
+        self.object.generate_application_number()
+        form = self.get_form()
+        form.instance = self.object
+
+        return super(ApplicationCreate, self).form_valid(form)
+
+
+class ApplicationDetail(generic.DetailView):
+    model = Application
+    context_object_name = 'application'
+
+
+class ApplicationList(LoginRequiredMixin, generic.ListView):
+    model = Application
+    login_url = '/login'
+
+
+def application_edit_handler(request, pk):
+    # allow logged in users to edit more fields
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('elefant:edit_admin', args=(pk,)))
+    else:
+        return HttpResponseRedirect(reverse('elefant:edit_user', args=(pk,)))
+
+
+class ApplicationEdit(generic.UpdateView):
+    model = Application
+    fields = ['applicant', 'contact', 'e_mail', 'description', 'total_amount']
+
+    def get_context_data(self, **kwargs):
+        # add application_number to context as headline
+        context = super(ApplicationEdit, self).get_context_data(**kwargs)
+        application_number = get_object_or_404(Application, pk=self.kwargs.get('pk')).application_number
+        context['headline'] = application_number
+        return context
+
+
+class ApplicationEditAll(LoginRequiredMixin, ApplicationEdit):
+    login_url = '/login'
+
+    def __init__(self):
+        super(ApplicationEditAll, self).__init__()
+        # add fields which should be editable by logged in users
+        self.fields = ['applicant', 'contact', 'e_mail', 'description', 'total_amount', 'status', 'budget_category']
---
+++ src/gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var del = require('del');
+var merge = require('merge-stream');
+
+
+gulp.task('default', function () {
+    return runSequence(
+        'build'
+    );
+});
+
+gulp.task('build', ['install-deps', 'sass'], function () {
+});
+
+gulp.task('build-clean', function () {
+    return del(['elefant/static/elefant/**/_*.scss', 'elefant/static/elefant/*.css', '!elefant/static/elefant/*min.css']); // delete build deps
+});
+
+gulp.task('install-deps', function () {
+    var icons = gulp.src(['node_modules/material-design-icons/iconfont/**', '!**/README.md', 'node_modules/material-design-icons/LICENSE']) // install icons
+        .pipe(gulp.dest('elefant/static/elefant/material-design-icons'));
+    var mdl_js = gulp.src(['node_modules/material-design-lite/*.min.js', 'node_modules/material-design-lite/LICENSE']) // install mdl js
+        .pipe(gulp.dest('elefant/static/elefant/material-design-lite'));
+    var roboto = gulp.src(['node_modules/roboto-slab-fontface-kit/**/*.ttf', 'node_modules/roboto-slab-fontface-kit/**/*.woff*', 'node_modules/roboto-slab-fontface-kit/**/*.scss']) // install roboto slab
+        .pipe(gulp.dest('elefant/static/elefant/fonts/roboto-slab'));
+    var raleway = gulp.src(['node_modules/typeface-raleway/*/*.eot', 'node_modules/typeface-raleway/**/*.woff*', 'node_modules/typeface-raleway/**/*.svg']) // install roboto slab
+        .pipe(gulp.dest('elefant/static/elefant/fonts/raleway'));
+    return merge(icons, mdl_js, roboto, raleway);
+});
+
+gulp.task('install-sass-build-deps', function () {
+    return mdl_css = gulp.src('node_modules/material-design-lite/material.css') // install mdl css
+        .pipe(rename("_material.scss"))
+        .pipe(gulp.dest('elefant/static/elefant/material-design-lite'));
+});
+
+gulp.task('sass', ['install-sass-build-deps'], function () {
+    return gulp.src('elefant/static/elefant/*.scss') // build sass
+        .pipe(sass())
+        .pipe(gulp.dest('elefant/static/elefant'))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({extname: '.min.css'}))
+        .pipe(gulp.dest('elefant/static/elefant'));
+});

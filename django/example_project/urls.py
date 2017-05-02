212020c34a0b76dd81f96ee1c234ae2f3ed2f616
Julian Haas <mail@julian-haas.de>
2017-05-02 15:26:57 +0200

    reorganize folder structure

--- django/example_project/urls.py
+++ django/example_project/urls.py
@@ -1,7 +1,7 @@
-"""elefant URL Configuration
+"""example_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls, name='admin'),
+    url(r'^admin/', admin.site.urls),
     url(r'^', include('elefant.urls')),
 ]
--- elefant/django_project/secret_settings.py
+++
@@ -1,9 +0,0 @@
-EMAIL_HOST = "smtp.strato.de"
-
-EMAIL_PORT = 465
-
-EMAIL_HOST_PASSWORD = "Eeghai?wei2CheQu"
-
-EMAIL_HOST_USER = "elefant@julian-haas.de"
-
-EMAIL_USE_SSL = True
